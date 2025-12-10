35eb09cc8ac70384da32f1ecb0a9799a6505d26a
Alee Kolachi <alee.kolachi@gmail.com>
2025-12-10 08:32:14 +0500

    Shifted model to the new file and updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+from models.convnet import ConvNet
 
 # -----------------------------
 # Device
@@ -52,50 +53,6 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, s
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
-# -----------------------------
-# Model Definition
-# -----------------------------
-class ConvNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),   # 16x16
-
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),   # 8x8
-
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2)    # 4x4
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(128*4*4, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, 10)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        return self.classifier(x)
 
 model = ConvNet().to(device)
 criterion = nn.CrossEntropyLoss()
