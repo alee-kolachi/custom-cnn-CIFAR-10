060b1b17940300234fdd2731919aeb9bbf3ab0b1
Alee Kolachi <alee.kolachi@gmail.com>
2025-12-10 08:38:31 +0500

    updated inference api file

--- inference_api.py
+++ inference_api.py
@@ -5,6 +5,7 @@ import torchvision.transforms as transforms
 from PIL import Image
 from fastapi import FastAPI, UploadFile, File
 import io
+from models.convnet import ConvNet
 
 app = FastAPI()
 
@@ -12,49 +13,6 @@ MODEL_PATH = "best_convnet_cifar10.pth"
 
 CLASSES = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
-
-#Model Definition
-class ConvNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),   # 16x16
-
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),   # 8x8
-
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2)    # 4x4
-        )
-
-        self.classifier = nn.Sequential(
-            nn.Linear(128*4*4, 256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, 10)
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        return self.classifier(x)
     
 #Load Model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
